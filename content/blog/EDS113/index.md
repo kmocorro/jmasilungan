7918b6e635e349192dca428e7fd7e3f483a591dd
kmocorro <kevinmocorro.github@gmail.com>
2020-09-24 10:35:51 +0800

    ok

--- content/blog/EDS113/index.md
+++ content/blog/EDS113/index.md
@@ -1,7 +1,7 @@
 ---
 title: "EDS113"
 date: "2020-09-24"
-description: "This is a short description of EDS113"
+description: "My eJournal on EDS113 Principles and Methods of Assessment"
 ---
 
 Hello world. This is EDS 113
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const BlogIndex = ({ data, location }) => {
   if (posts.length === 0) {
     return (
       <Layout location={location} title={siteTitle}>
-        <SEO title="All posts" />
+        <SEO title="Welcome" />
         <Bio />
         <p>
           No blog posts found. Add markdown posts to "content/blog" (or the
@@ -25,7 +25,7 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
+      <SEO title="Welcome" />
       <Bio />
       {posts.map(post => {
         const title = post.frontmatter.title || post.fields.slug
